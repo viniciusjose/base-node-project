33020f9195c1aa300efa9496ab84a435ebb115d9
Vinicius Silva <vinicius.silva@telium.com.br>
2023-01-17 00:20:20 -0300

    finish node base project

---
+++ src/application/controllers/ExampleController.ts
@@ -0,0 +1,9 @@
+export class ExampleController {
+  run (): string {
+    return 'run'
+  }
+}
+
+const example = new ExampleController()
+
+example.run()
---
+++ tests/example.spec.ts
@@ -0,0 +1,5 @@
+describe('example test', () => {
+  it('example', function () {
+    expect(1).toBe(1)
+  })
+})
